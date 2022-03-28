5656c2dda5752dc4ef597ece1d2c1053f4130de2
Daniel da silva pedro <danielsp699@outlook.com>
2022-03-28 18:44:17 -0300

    End of class ZIPFile

--- 3 - modulosPython/compactarDescompactarArquivos/main.py
+++ 3 - modulosPython/compactarDescompactarArquivos/main.py
@@ -1,18 +1,22 @@
 from zipfile import ZipFile
 import os
 
-caminho = r'C:\Workspaces\ws-python\3 - modulosPython\compactarDescompactarArquivos\arquivosparacompactar'
+caminho = r'DIRETORIO_A_COMPACTAR'
 
 # Gerando o arquivo ZIP com permissao para escrita
-with ZipFile('arquivo.zip', 'w') as zip:
+with ZipFile('arquivo.zip', 'w') as compactando:
     #  Navegando pela pasta com listdir para não entrar em subpastas
     for arquivo in os.listdir(caminho):
         caminhoCompleto = os.path.join(caminho, arquivo)  # Pegando Caminho do arquivo + nome do arquivo aqui
-        zip.write(caminhoCompleto, arquivo)  # colocando no arquivo ZIP sem colocar no nome do arquivo o caminho
+        compactando.write(caminhoCompleto, arquivo)  # colocando no arquivo ZIP sem colocar no nome do arquivo o caminho
         # por isso a variável arquivo ali (caminhocompleto = Onde pegar o arquivo, arquivo = nome do arquivo dentro
         # do zip)
 
 # Lendo os arquivos dentro do ZIP
-with ZipFile('arquivo.zip', 'r') as zip:
-    for arquivo in zip.namelist():
+with ZipFile('arquivo.zip', 'r') as lendo:
+    for arquivo in lendo.namelist():
         print(arquivo)
+
+# Descompactando os itens
+with ZipFile('arquivo.zip', 'r') as descompactando:
+    descompactando.extractall('descompactado')
