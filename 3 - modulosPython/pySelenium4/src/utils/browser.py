467b6972e6a1a9f34b220d2a121fd233a98dff6b
Daniel da silva pedro <danielsp699@outlook.com>
2022-03-31 16:20:59 -0300

    Update Selenium 4 - New class

---
+++ 3 - modulosPython/pySelenium4/src/utils/browser.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from time import sleep
+
+# Caminho para a raiz do projeto
+# Parent volta uma pasta igual ao (../)
+ROOT_FOLDER = Path(__file__).parent.parent.parent
+# print(ROOT_FOLDER)
+# Caminho para a pasta onde o chromedriver está
+CHROME_DRIVER_PATH = ROOT_FOLDER / 'bin' / 'chromedriver.exe'
+
+
+# print(CHROME_DRIVER_PATH)
+
+# Instanciando o Chrome
+def makeChromeBrowser(*options: str) -> webdriver.chrome:
+    # Chamando o chromeOptions
+    chrome_options = webdriver.ChromeOptions()
+
+    # chrome_options.add_argument('--headless')
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+        # Criando o serviço do Chrome com o caminho do ChromeDriver
+        chrome_service = Service(
+            executable_path=CHROME_DRIVER_PATH
+        )
+    # Criando o browser com as opçoes e serviço configurado
+    browser = webdriver.Chrome(
+        service=chrome_service,
+        options=chrome_options
+    )
+    # Retorno o resultado
+    return browser
+
+
+if __name__ == '__main__':
+    options = ('--disable-gpu', '--no-sandbox')  # Criando options de exemplo
+    browser = makeChromeBrowser(*options) # instanciando com as options que criei acima
+
+    # Utilizando o selenium
+    browser.get('https://www.google.com/')
+    sleep(5)
+    browser.quit()
